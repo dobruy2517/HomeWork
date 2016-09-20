6cb57b0d92f27d0dc9a0d2d605d2050a2a2be4de
Ihor Dobrovolskyi <Ihor.dobrovolskyi@gmail.com>
2016-09-20 21:51:39 +0300

    Initial commit

---
+++ src/packageAdin/OurSuperMegaClass.java
@@ -0,0 +1,20 @@
+package packageAdin;
+
+/**
+ * Created by AID on 20.09.2016.
+ */
+public class OurSuperMegaClass {
+
+    public static void main(String[] args) {
+        System.out.println("PISH-PASH");
+        System.out.println("OLOLOLO");
+        int a = 5;
+        int b=16;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+
+}
