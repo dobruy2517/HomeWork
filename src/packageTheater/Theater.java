dcebdf42be182e1b2e27daafaa69c437f18d5379
Igor <ihor.dobrovolskyi@gmail.com>
2016-09-28 17:47:16 +0300

    Причесал!)

--- src/packageTheater/Theater.java
+++ src/packageTheater/Theater.java
@@ -7,20 +7,20 @@ import java.util.Scanner;
  */
 public class Theater {
     public static void main(String[] args) {
-        Scanner vvod=new Scanner(System.in);
-        long m=vvod.nextLong();
-        long n=vvod.nextLong();
-        long a=vvod.nextLong();
-        long quantityLenght=0;
-        long quantityWeight=0;
-        if (m%n==0){
-            quantityLenght=m/a;
-            quantityWeight=n/a;
-            System.out.println(quantityLenght*quantityWeight);
-        } else{
-            quantityLenght=(m/a)+1;
-            quantityWeight=(n/a)+1;
-            System.out.println(quantityLenght*quantityWeight);
+        Scanner vvod = new Scanner(System.in);
+        long m = vvod.nextLong();
+        long n = vvod.nextLong();
+        long a = vvod.nextLong();
+        long quantityLenght = 0;
+        long quantityWeight = 0;
+        if (m % n == 0) {
+            quantityLenght = m / a;
+            quantityWeight = n / a;
+            System.out.println(quantityLenght + quantityWeight);
+        } else if (m % n != 0) {
+            quantityLenght = (m / a) + 1;
+            quantityWeight = (n / a) + 1;
+            System.out.println(quantityLenght + quantityWeight);
         }
 
     }
