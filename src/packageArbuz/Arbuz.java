e1b93457cfb3ebe889a51109713c7c10098c318a
Igor <ihor.dobrovolskyi@gmail.com>
2016-09-29 16:05:58 +0300

    Задача из CodeForces

---
+++ src/packageArbuz/Arbuz.java
@@ -0,0 +1,21 @@
+package packageArbuz;
+
+import java.util.Scanner;
+
+/**
+ * Created by konstruktor on 29.09.2016.
+ */
+public class Arbuz {
+    public static void main(String[] args) {
+        Scanner vvod = new Scanner(System.in);
+        short w = vvod.nextShort();
+        short n = 2;
+        if ((w % 2 == 0) & (w > 2)) {
+            System.out.println("YES");
+        } else if (w == 2) {
+            System.out.println("NO");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
